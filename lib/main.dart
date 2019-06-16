d79df0c936611229731f55601b0df03278947711
jelena <gorbova.jelena@gmail.com>
2019-06-16 23:28:07 +0300

    import firebase packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:neverlate/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/firebase_core.dart';
 
 void main() => runApp(MyApp());
 
